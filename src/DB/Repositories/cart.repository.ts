b52c85caca2c699f6222831b7251fb944d4b1c0b
Tawfik Tamer <tawfiktamer.dev@gmail.com>
2025-11-22 20:35:00 +0200

    refactor: clear the cart apis

--- src/DB/Repositories/cart.repository.ts
+++
@@ -1,9 +0,0 @@
-import { ICart } from "../../Common";
-import { cartModel } from "../models";
-import { BaseRepository } from "./base.repository";
-
-export class CartRepository extends BaseRepository<ICart> {
-  constructor() {
-    super(cartModel);
-  }
-}
